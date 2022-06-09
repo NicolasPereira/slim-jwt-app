1333afdeef8228d87482c5fc164dd731ed97509d
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2022-06-09 03:10:03 -0300

    feat: adicionando configuração de db

--- config/settings.php
+++ config/settings.php
@@ -31,13 +31,33 @@ $settings['error'] = [
 ];
 
 $settings['db'] = [
-    'driver' => 'pgsql',
-    'host' => 'slim_db',
-    'username' => 'root',
-    'database' => 'slim_app',
-    'password' => '',
-    'charset' => 'utf8mb4',
-    'collation' => 'utf8mb4_unicode_ci',
+    // Enables or disables Doctrine metadata caching
+    // for either performance or convenience during development.
+    'dev_mode' => true,
+
+    // Path where Doctrine will cache the processed metadata
+    // when 'dev_mode' is false.
+    'cache_dir' => __DIR__ . '/var/doctrine',
+
+    // List of paths where Doctrine will search for metadata.
+    // Metadata can be either YML/XML files or PHP classes annotated
+    // with comments or PHP8 attributes.
+    'metadata_dirs' => [__DIR__ . '/src/Domain'],
+
+    // The parameters Doctrine needs to connect to your database.
+    // These parameters depend on the driver (for instance the 'pdo_sqlite' driver
+    // needs a 'path' parameter and doesn't use most of the ones shown in this example).
+    // Refer to the Doctrine documentation to see the full list
+    // of valid parameters: https://www.doctrine-project.org/projects/doctrine-dbal/en/current/reference/configuration.html
+    'connection' => [
+        'driver' => 'pdo_pgsql',
+        'host' => 'slim_db',
+        'port' => 5432,
+        'dbname' => 'slim_app',
+        'user' => 'root',
+        'password' => '',
+        'charset' => 'utf-8'
+    ]
 ];
 
 return $settings;
