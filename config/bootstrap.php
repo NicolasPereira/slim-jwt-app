1270a476bffa576db21b3b1a9689955d8f5ef03d
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2022-06-11 09:32:50 -0300

    feat: adicionando arquivo dependencies para a construição do container de injeção de dependencia

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -9,6 +9,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $containerBuilder = new ContainerBuilder();
 
 // Set up settings
+$containerBuilder->addDefinitions(__DIR__ . '/dependencies.php');
 $containerBuilder->addDefinitions(__DIR__ . '/container.php');
 // Build PHP-DI Container instance
 $container = $containerBuilder->build();
