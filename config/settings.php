1864bd9d2234a27e424fbbf3b6b913602066b37b
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2022-06-15 23:23:24 -0300

    fix: corrigindo o apontamento do caminho para os arquivos temporarios do doctrine

--- config/settings.php
+++ config/settings.php
@@ -37,7 +37,7 @@ $settings['db'] = [
 
     // Path where Doctrine will cache the processed metadata
     // when 'dev_mode' is false.
-    'cache_dir' => __DIR__ . '/var/doctrine',
+    'cache_dir' => __DIR__ . '../app/var/doctrine',
 
     // List of paths where Doctrine will search for metadata.
     // Metadata can be either YML/XML files or PHP classes annotated
