c1942b543de800668fbff0c272e0f7d5b465fc62
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2022-06-15 23:22:42 -0300

    fix: corrigindo configuração que aponta para onde estão as anotações do Doctrine

--- config/settings.php
+++ config/settings.php
@@ -42,7 +42,7 @@ $settings['db'] = [
     // List of paths where Doctrine will search for metadata.
     // Metadata can be either YML/XML files or PHP classes annotated
     // with comments or PHP8 attributes.
-    'metadata_dirs' => [__DIR__ . '/src/Domain'],
+    'metadata_dirs' => [__DIR__.'/../app/Domain'],
 
     // The parameters Doctrine needs to connect to your database.
     // These parameters depend on the driver (for instance the 'pdo_sqlite' driver
