14b99def06ee30b562d10a2a5d3966953d26235e
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2022-06-17 20:58:40 -0300

    feat: criando a Interface

--- app/Domain/User/Repository/UserCreatorRepository.php
+++ app/Domain/User/Repository/UserCreatorRepository.php
@@ -1,26 +1,7 @@
 <?php
 namespace App\Domain\User\Repository;
 
-use App\Domain\User\User;
-use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\Exception\ORMException;
-use Doctrine\ORM\OptimisticLockException;
-
-class UserCreatorRepository
+interface UserCreatorRepository
 {
-    public function __construct(private EntityManager $em)
-    {
-    }
-
-    /**
-     * @throws OptimisticLockException
-     * @throws ORMException
-     */
-    public function insert(array $user): int
-    {
-        $userCreate = new User($user['name'], $user['email'], $user['password']);
-        $this->em->persist($userCreate);
-        $this->em->flush();
-        return $userCreate->id;
-    }
+    public function insert(array $user): int;
 }
