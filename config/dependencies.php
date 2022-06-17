b6d28c3badb6150603aab4d6620f89279fbf6351
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2022-06-17 20:59:00 -0300

    feat: implementando a inversão de controle

--- config/dependencies.php
+++ config/dependencies.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Domain\User\Repository\UserCreatorRepository;
+use App\Infrastructure\DoctrineCreateUserRepository;
 use App\Infrastructure\Factory\EntityManagerFactory;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
@@ -9,4 +11,5 @@ return [
     EntityManager::class => DI\factory(function(ContainerInterface $container): EntityManagerInterface {
         return EntityManagerFactory::create($container);
     }),
+   UserCreatorRepository::class => \DI\autowire(DoctrineCreateUserRepository::class)
 ];
