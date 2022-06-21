f03266dc892cc0eff290bba59bcf56e2a167861e
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2022-06-21 01:49:15 -0300

    feat: adicionando o CreateUserDTO como parametro

--- app/Domain/User/Repository/UserCreatorRepository.php
+++ app/Domain/User/Repository/UserCreatorRepository.php
@@ -1,7 +1,9 @@
 <?php
 namespace App\Domain\User\Repository;
 
+use App\Domain\User\DTO\CreateUserDTO;
+
 interface UserCreatorRepository
 {
-    public function insert(array $user): int;
+    public function insert(CreateUserDTO $userDTO): int;
 }
