e4cb23ac167d03bf3fbbea8c9dbea3a3e1b46ce0
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2022-06-28 00:03:20 -0300

    feat: adicionando controller para login

---
+++ app/Action/LoginAction.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Action;
+
+use App\Domain\Auth\DTO\UserLoginDTO;
+use App\Domain\Auth\Service\AuthenticateUser;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class LoginAction
+{
+    public function __construct(private AuthenticateUser $authenticateUser)
+    {
+    }
+
+    public function __invoke(
+        ServerRequestInterface $request,
+        ResponseInterface $response
+    ): ResponseInterface
+    {
+        $user = json_decode($request->getBody(), true);
+        $userLoginDTO = UserLoginDTO::create($user);
+        $auth = $this->authenticateUser->validateUser($userLoginDTO);
+        $result = [
+            'token' => $auth
+        ];
+        $response->getBody()->write(json_encode($result));
+
+        return $response
+            ->withHeader('Content-Type', 'application/json')
+            ->withStatus(201);
+    }
+}
