ba3409caf40d621ef5c4f2162ba69db6077480d5
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2022-06-28 00:02:10 -0300

    feat: alterando nome do arquivo

--- app/Domain/User/Repository/UserRepository.php
+++ app/Domain/User/Repository/UserRepository.php
@@ -3,7 +3,9 @@ namespace App\Domain\User\Repository;
 
 use App\Domain\User\User;
 
-interface UserCreatorRepository
+interface UserRepository
 {
     public function insert(User $user): int;
+
+    public function findBy(string $property, string $valueProperty): User | null;
 }
--- app/Infrastructure/DoctrineUserRepository.php
+++ app/Infrastructure/DoctrineUserRepository.php
@@ -1,14 +1,13 @@
 <?php
 namespace App\Infrastructure;
 
-use App\Domain\User\DTO\CreateUserDTO;
-use App\Domain\User\Repository\UserCreatorRepository;
+use App\Domain\User\Repository\UserRepository;
 use App\Domain\User\User;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Exception\ORMException;
 use Doctrine\ORM\OptimisticLockException;
 
-class DoctrineCreateUserRepository implements UserCreatorRepository
+class DoctrineUserRepository implements UserRepository
 {
     public function __construct(private EntityManager $em)
     {
@@ -23,5 +22,10 @@ class DoctrineCreateUserRepository implements UserCreatorRepository
         $this->em->persist($user);
         $this->em->flush();
         return $user->id;
+    }
+
+    public function findBy(string $property, string $valueProperty): User | null
+    {
+        return $this->em->getRepository(User::class)->findOneBy(array($property => $valueProperty));
     }
 }
